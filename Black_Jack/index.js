618fc969c16bbee43d1e0dc4d112137f8e93d859
MirAfzalul <mirafzalulbashar@gmail.com>
2022-04-01 23:49:22 -0400

    points condition

--- Black_Jack/index.js
+++ Black_Jack/index.js
@@ -3,3 +3,13 @@ let x=Math.floor((Math.random()*10)+2);
 let y=Math.floor((Math.random()*10)+2);
 let sum=x+y;
 console.log(x +" |"+ y+"sum:"+sum);
+let hasBlackJack=false;
+
+if (sum<=20){
+    console.log("Do you want to draw a new card?")
+}else if(sum===21){
+    console.log("wohoo BLACK JACK")
+}
+else{
+    console.log("Out of the game!")
+}
