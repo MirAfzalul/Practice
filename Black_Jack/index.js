7a59fdd150b2e20f8d9d52d4efe66f5b9804cb15
MirAfzalul <mirafzalulbashar@gmail.com>
2022-04-08 00:14:47 -0400

    added player name

--- Black_Jack/index.js
+++ Black_Jack/index.js
@@ -1,31 +1,39 @@
 let hasBlackJack=false;
-let isAlive=true;
+let isAlive=false;
 let message="";
 let getCardElement=document.getElementById("cardElement");
 let getSumElement=document.getElementById("sumElement");
 let getMessageElement=document.getElementById("message-el");
 let getNewCardBtnElement=document.querySelector("#newCardBtn");
 let cards=[];
-let sum=0;
+let x,y,zsum=0;
+let player={
+        name:"Asif",
+        chips:"$250"
+}
+
+document.getElementById("playerElement").textContent=player.name+": "+player.chips;
 function startGame(){
-    let x=Math.floor((Math.random()*10)+2);
-    let y=Math.floor((Math.random()*10)+2);
-    cards.push(x,y);
+    card=[];
+    isAlive=true;
+    x=Math.floor((Math.random()*10)+2);
+    y=Math.floor((Math.random()*10)+2);
+    cards=[x,y];
     sum=x+y;
     getCardElement.textContent="Cards: ";
-    renderGame(sum);
-    //cards.pop(x);
+    renderGame();
+  //  cards.pop(x);
     //cards.pop(y);
 }
-function renderGame(s){
+function renderGame(){
     for(let i=0;i<cards.length;i++){
         getCardElement.textContent+=" "+cards[i]+" ";
     }
 //getCardElement.textContent="Cards: "+cards[0]+" "+cards[1];
-getSumElement.textContent="Sum: "+s;
-if (s<=20){
+getSumElement.textContent="Sum: "+sum;
+if (sum<=20){
     message=("Do you want to draw a new card?")
-}else if(s===21){
+}else if(sum===21){
     message=("wohoo BLACK JACK")
     hasBlackJack=true;
     
@@ -33,7 +41,7 @@ if (s<=20){
 else{
     message=("Out of the game!")
     isAlive=false;
-    //getNewCardBtnElement.remove();
+    //getNewCardBtnElement.style.visibility='hidden';
 
 }
 getMessageElement.textContent=message
@@ -44,13 +52,15 @@ console.log(cards);
 }
 
 function DrawNewCard(){
-    let z=Math.floor((Math.random()*10)+2);
+    if(isAlive===true && hasBlackJack===false){
+    z=Math.floor((Math.random()*10)+2);
     sum+=z;
     //getCardElement.textContent="Cards:"+x+"and"+y+"and"+z;
     //getSumElement.textContent="Sum:"+sum;
     cards.push(z);
     getCardElement.textContent="Cards: ";
     console.log(cards);
-    renderGame(sum);
-    cards.pop(z);
+    renderGame();
+    //cards.pop(z);
+    }
 }
