1df11210f843a9acb45007349003c55fe160a698
MirAfzalul <mirafzalulbashar@gmail.com>
2022-04-08 18:27:48 -0400

    Chrome Extension template

---
+++ Chrome Extension/index.js
@@ -0,0 +1,6 @@
+let inputBtn=document.getElementById("input-btn");
+let myLeads =[];
+let input=document.getElementById=("input-el");
+inputBtn.addEventListener("click",function(){
+    console.log("clicked")
+})
