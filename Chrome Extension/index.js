3438daf1f6c37de86c1f7b69de4b03e43e0e3e5e
MirAfzalul <mirafzalulbashar@gmail.com>
2022-04-09 01:28:58 -0400

    Render leads

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,6 +1,21 @@
-let inputBtn=document.getElementById("input-btn");
+const inputBtn=document.getElementById("input-btn");
 let myLeads =[];
-let input=document.getElementById=("input-el");
+const inputEl=document.querySelector("#input-el");
+const ulEl=document.querySelector("#ul-el");
+//console.log(inputEl);
+
 inputBtn.addEventListener("click",function(){
-    console.log("clicked")
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
+    //ulEl.textContent=myLeads+"\n";
+   renderLeads();
 })
+function renderLeads(){
+    let listItems="";
+    for(let i=0;i<myLeads.length;i++){
+        listItems+="<li>"+myLeads[i]+"</li>";
+    }
+    ulEl.innerHTML=listItems;
+    
+}
+//console.log(myLeads);
