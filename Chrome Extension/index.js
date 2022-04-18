c2ef3cbd514e4d58b85dc535c5e2471c9e191e92
MirAfzalul <mirafzalulbashar@gmail.com>
2022-04-17 23:08:07 -0400

    store array in local storage

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -3,13 +3,25 @@ let myLeads =[];
 const inputEl=document.querySelector("#input-el");
 const ulEl=document.querySelector("#ul-el");
 //console.log(inputEl);
-
+localStorage.clear();
+//myLeads=JSON.parse(myLeads);;
+let leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"));
+//let checkLeadStorage= Boolean(leadsFromLocalStorage);
+console.log(leadsFromLocalStorage);
+if(leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage;
+    renderLeads();
+}
 inputBtn.addEventListener("click",function(){
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    //console.log(myLeads);
     //ulEl.textContent=myLeads+"\n";
-   renderLeads();
    inputEl.value="";
+localStorage.setItem("myLeads",JSON.stringify(myLeads));
+//let storeNames=JSON.parse(localStorage.getItem("myLeads"))
+   
+    renderLeads();
+   
 })
 function renderLeads(){
     let listItems="";
@@ -20,7 +32,7 @@ function renderLeads(){
             </a>
         </li>`;
     }
-    console.log(listItems);
+    //console.log(listItems);
     ulEl.innerHTML=listItems;
     
 }
